1a83a6056bec9e93b4fa0c04a286ce8d85ae947d
Vlad Horii <vladhorii@yandex.ru>
2021-06-25 16:38:34 +0300

    Перенес логику в переменную

--- src/components/friendsList/FriendsListItem.js
+++ src/components/friendsList/FriendsListItem.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import s from './FriendsList.module.css';
 
 export default function FriendsListItem({ avatar, name, isOnline }) {
+  const onlineClassNames = `${s.status} ${isOnline ? s.online : s.offline}`;
   return (
     <div className={s.item}>
-      <span className={`${s.status} ${isOnline ? s.online : s.offline}`}></span>
+      <span className={onlineClassNames}></span>
       <img className={s.avatar} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
     </div>
