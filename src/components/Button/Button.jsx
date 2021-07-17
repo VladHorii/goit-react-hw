ca79beda13e4434270dff2781eeb780702e57970
Vlad Horii <vladhorii@yandex.ru>
2021-07-17 14:41:45 +0300

    Убрал функцию-обёртку

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,9 @@
 import { Button } from './Button.styled';
 
-async function onClickToBtn(callback) {
-  const res = await callback();
-
-  if (res) {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-  }
-}
 export default function ButtonLoadMore({ onClick }) {
   return (
-    <>
-      <Button type="button" onClick={() => onClickToBtn(onClick)}>
-        Load more
-      </Button>
-    </>
+    <Button type="button" onClick={onClick}>
+      Load more
+    </Button>
   );
 }
