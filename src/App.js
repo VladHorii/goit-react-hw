07ef06bd9271a541afaed3e82434cc9e8be0843b
Vlad Horii <vladhorii@yandex.ru>
2021-07-26 19:08:30 +0300

    Добавил react load lazy и react suspense

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
+import { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import { useState } from 'react';
 
 import Container from './components/Container';
-import HomePage from './views/HomePage';
-import MoviesPage from './views/MoviesPage';
 import Navigation from './components/Navigation';
-import MovieDetailsPage from './views/MovieDetailsPage';
-import NotFoundPage from './views/NotFoundPage';
-// import Searchbar from './components/Searchbar';
-
+import LoaderSpinner from './components/LoaderSpinner';
+const HomePage = lazy(() => import('./views/HomePage'));
+const MoviesPage = lazy(() => import('./views/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./views/MovieDetailsPage'));
+const NotFoundPage = lazy(() => import('./views/NotFoundPage'));
 export default function App() {
-  // const [moviesList, setMoviesList] = useState([]);
-
   return (
     <>
-      {/* <Searchbar /> */}
-
       <Container>
         <Navigation />
 
         <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/movies" exact>
-            <MoviesPage />
-          </Route>
-          <Route path="/movies/:movieId">
-            <MovieDetailsPage />
-          </Route>
-          <Route>
-            <NotFoundPage />
-          </Route>
+          <Suspense fallback={<LoaderSpinner />}>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/movies" exact>
+              <MoviesPage />
+            </Route>
+            <Route path="/movies/:movieId">
+              <MovieDetailsPage />
+            </Route>
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Suspense>
         </Switch>
       </Container>
     </>
